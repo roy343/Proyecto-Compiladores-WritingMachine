73517c501aef257c816872e000821bc8c2efb105
Gabriel González <37197284+gabogh123@users.noreply.github.com>
2021-03-30 02:54:26 -0600

    Analizador Semántico

---
+++ Proyecto/Semantic.py
@@ -0,0 +1,19 @@
+def runSemanticAnalizer(parse):
+    try:
+        inicio(parse[1])
+        procedimientos(parse[3])
+    except:
+        print('error de estructura')
+
+
+def inicio(body):
+    print(body)
+    print(1)
+    for i in body:
+        print(i)
+        print('\n')
+    print(2)
+
+
+def procedimientos(body):
+    print(body)
